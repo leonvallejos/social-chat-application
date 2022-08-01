754c030632840867acfea8bef2175568836922a2
Leon Vallejos <dev.leonvallejos@gmail.com>
2022-08-01 03:14:15 -0300

    Add import http, morgan, socket.io, cors

--- server/index.js
+++ server/index.js
@@ -1,8 +1,14 @@
 import express from "express";
 import morgan from 'morgan';
+import { Server as SocketServer } from "socket.io";
+import http from 'http'
+import cors from 'cors'
 
 const app = express();
+const server = http.createServer(app)
+const io = new SocketServer(server)
 
+app.use(cors())
 // morgan for terminal dump info
 app.use(morgan('dev'))
 

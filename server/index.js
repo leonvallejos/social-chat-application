c906f058ad2bd95bb1b49e357bc830bb807b95e4
Leon Vallejos <dev.leonvallejos@gmail.com>
2022-07-31 23:01:49 -0300

    Add morgan for terminal

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 import express from "express";
+import morgan from 'morgan';
 
 const app = express();
+
+// morgan for terminal dump info
+app.use(morgan('dev'))
 
 app.listen(3000);
 console.log('Server started on port 3000')

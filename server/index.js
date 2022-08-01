cbabacb8b2c998f88a91d2c84ab8918428a14b40
Leon Vallejos <dev.leonvallejos@gmail.com>
2022-08-01 07:34:07 -0300

    Add universal port in backend

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import morgan from 'morgan';
 import { Server as SocketServer } from "socket.io";
 import http from 'http'
 import cors from 'cors'
+import {PORT} from './config.js'
 
 const app = express();
 const server = http.createServer(app)
@@ -12,5 +13,5 @@ app.use(cors())
 // morgan for terminal dump info
 app.use(morgan('dev'))
 
-app.listen(3000);
-console.log('Server started on port 3000')
+app.listen(PORT);
+console.log('Server started on port ', PORT);

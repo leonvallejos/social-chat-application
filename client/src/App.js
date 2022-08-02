824b43cdcd45f74bd361fe85f46a2e018a4aed87
Leon Vallejos <dev.leonvallejos@gmail.com>
2022-08-02 19:21:26 -0300

    Capture of events in the form

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,23 @@
 import './App.css';
 import io from 'socket.io-client';
 import React from 'react'
+import { useState } from 'react';
 
 // eslint-disable-next-line
 const socket = io('http://localhost:4000'); 
 
 function App() {
+    const [message, setMessage] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(message);
+    }
+
   return (
     <div className="App">
-      <form>
-        <input type="text" />
+      <form onSubmit={handleSubmit}> 
+        <input type="text" onChange={e => setMessage(e.target.value)}/>
         <button>Send</button>
       </form>
     </div>
